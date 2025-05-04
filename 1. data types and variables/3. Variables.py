fa85ab47290add79f9047f3b2aff014273fcdd80
Manav <manav.gcet92@outlook.com>
2025-05-04 15:59:32 +0530

    @f608253 changes 4 May
    Signed-off-by: Manav <manav.gcet92@outlook.com>

---
+++ 1. data types and variables/3. Variables.py
@@ -0,0 +1,28 @@
+# 1. Create a variable named 'age' and assign it to your age as an integer.
+age = 27
+
+# 2. Create a variable called 'height" and assign it to your height in meters as a float.
+height = 1.85
+
+# 3. Create a variable called 'name' and assign it to your name as a string.
+name = 'Manav'
+
+# 4. Create a variable called 'is_student' and assign it a boolean value indicating whether you are a student or not.
+is_student = False
+
+# 5. Print out all these variable.
+print(f"Original values: Age: {age}, Height: {height}, Name: {name}, Is Student: {is_student}")
+
+# 6. Increase the 'age' variable by 1 and print it out.
+age = age + 1
+
+# 7. Multiply the 'height' variable by 100 to convert it to centimeters and print it out.
+height = height * 100
+
+# 8. Add "is my name" to the 'name' variable and print it out.
+name = name + " is my name"
+
+# 9. Change 'Is_student' to the opposite and print it out.
+is_student = True
+
+print(f"Modified values: Age: {age}, Height: {height}, Name: {name}, Is Student: {is_student}")
