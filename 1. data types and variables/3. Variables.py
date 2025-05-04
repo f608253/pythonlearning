05ab83439dca0171ec97ac58712765a33465ffd4
Manav <manav.gcet92@outlook.com>
2025-05-04 16:44:05 +0530

    @f608253 changes 4 May
    Signed-off-by: Manav <manav.gcet92@outlook.com>

--- 1. data types and variables/3. Variables.py
+++ 1. data types and variables/3. Variables.py
@@ -31,3 +31,34 @@ print(f"Modified values: Age: {age}, Height: {height}, Name: {name}, Is Student:
 # C:\Users\MANVENDRA\PycharmProjects\pythonProject\.venv\Scripts\python.exe "C:\Users\MANVENDRA\PycharmProjects\PythonLearning\1. data types and variables\3. Variables.py"
 # Original values: Age: 27, Height: 1.85, Name: Manav, Is Student: False
 # Modified values: Age: 28, Height: 185.0, Name: Manav is my name, Is Student: True
+
+#Basic variable naming rules:
+
+#Start with a letter or underscore (a-z, A-Z,  (_))
+age = 25
+_secret = "Shhh!"
+#2fast = "Speedy!" #This is not a correct variable name
+
+#Use letters , numbers or underscore (az , a_)
+user_age_2 = 32
+best_friend_1 = "Alice"
+#my-variable = 10  # Hyphens are not allowed in variable name
+
+#Case sensitive
+Age = 25
+age = 27
+AGE = 30
+
+print(Age)
+print(age)
+print(AGE)
+
+#No reserved keywords
+#from = "NYJ"  # Not a valid variable name
+#if = 5        # This too
+print("Hello JI")
+
+
+
+
+
