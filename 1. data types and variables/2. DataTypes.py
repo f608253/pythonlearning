ee98f4a084413b1420ac1282848a3c56181093f6
Manav <manav.gcet92@outlook.com>
2025-05-04 13:54:31 +0530

    @f608253 changes 4 May
    Signed-off-by: Manav <manav.gcet92@outlook.com>

---
+++ 1. data types and variables/2. DataTypes.py
@@ -0,0 +1,27 @@
+#Understanding data types in python
+
+# Common data tyoes
+
+#  Numbers
+#3,6,9 #  Integers (int)
+#3.14, -0.12 #  Floats (float)
+
+#Text
+#'This is a string' string(str)
+
+#Boolean
+#True,False boolean(bool)
+
+age = 29
+pi = 3.14
+name = 'Helen'
+is_new = True
+
+#Check data type
+#type()
+
+print(type(age))
+print(type(pi))
+print(type(name))
+print(type(is_new))
+
