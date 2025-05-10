ed533191934abf1933a0c4bd5a2d731f28f3654d
Manav <manav.gcet92@outlook.com>
2025-05-10 23:11:09 +0530

    @f608253 changes 10 May
    Signed-off-by: Manav <manav.gcet92@outlook.com>

--- Strings/strings-examples.py
+++ Strings/strings-examples.py
@@ -1,1 +1,17 @@
 #Word game script
+
+word = input("Enter a word: ")
+
+word_length = len(word)
+print(f"The word {word} has {word_length} characters")
+
+#reverse the word
+
+reversed_word = word[::-1]
+
+print(f"The reversed word is {reversed_word}")
+
+#create new word
+first_car = word[0]
+new_word = first_car * word_length
+print(f"A new word: {new_word}")
