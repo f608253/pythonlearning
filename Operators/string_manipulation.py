e03a02e5b8e51818a8e0800ee7d9c79b1f035c55
Manav <manav.gcet92@outlook.com>
2025-05-11 09:48:55 +0530

    @f608253 changes 11 May
    Signed-off-by: Manav <manav.gcet92@outlook.com>

---
+++ Operators/string_manipulation.py
@@ -0,0 +1,15 @@
+# Create a variable first_name and assign to it your first name.
+first_name = "Manav"
+# Create a variable last_name and assign to it your last name.
+last_name = "Yadav"
+# Create a variable full_name by concatenating first_name and last_name with a space in between.
+full_name = first_name + ' ' + last_name
+#  Print out the full_name.
+print(f"Full Name is: {full_name}")
+# Print out the length of full_name using the len()function.
+f_length = len(full_name)
+print(f"Full name length is: {f_length}")
+# Print out the full_name in all uppercase letters.
+print(f"Name in Uppercase is: {full_name.upper()}")
+# Print out the full_name in all lowercase letters.
+print(f"Name in lowercase is: {full_name.lower()}")
