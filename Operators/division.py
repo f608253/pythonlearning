fa866b41c23b8dde1340898cb6773dfc4fa194eb
Manav <manav.gcet92@outlook.com>
2025-05-11 20:41:47 +0530

    @f608253 changes 11 May
    Signed-off-by: Manav <manav.gcet92@outlook.com>

---
+++ Operators/division.py
@@ -0,0 +1,21 @@
+# Distributing Books
+
+# Task 1: Setting up variables
+number_of_books = 23
+number_of_students = 5
+
+# Task 2: Calculate books' distribution
+books_per_student = number_of_books // number_of_students
+
+# Task 3: Print out the result
+print(f"Each student will get {books_per_student} books.")
+
+#Modulo operator
+
+print(13 % 5)   #Output: 3
+print(14 % 5)   #Output: 4
+
+#Base ** exponent
+print(2 ** 4) #16
+print(3 ** 2) #9
+print(3 ** 3) #27
