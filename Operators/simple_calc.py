4e76cd0d89d125608dd7462f481c175cf3af0738
Manav <manav.gcet92@outlook.com>
2025-05-11 09:42:22 +0530

    @f608253 changes 11 May
    Signed-off-by: Manav <manav.gcet92@outlook.com>

---
+++ Operators/simple_calc.py
@@ -0,0 +1,17 @@
+#Create 2 variables num1 and num2 and assign them the integer values
+num1 = 10
+num2 = 20
+#2. Perform the following
+#Addition
+print(f"Addition: {num1 + num2}")
+#Sub
+print(f"Substraction: {num1 - num2}")
+#Multiplication
+print(f"Multiplication: {num1 * num2}")
+#Division
+print(f"Division: {num1 / num2}")
+#Integer value
+print(f"Integer division: {num1 // num2}")
+#Remainder
+print(f"Remainder: {num1 % num2}")
+
--- Strings/strings-examples.py
+++ Strings/strings-examples.py
@@ -15,3 +15,8 @@ print(f"The reversed word is {reversed_word}")
 first_car = word[0]
 new_word = first_car * word_length
 print(f"A new word: {new_word}")
+
+#concatenate
+suffix = 'ish'
+word_with_suffix = word + suffix
+print(f"The word with suffix is: '{suffix}': {word_with_suffix}")
